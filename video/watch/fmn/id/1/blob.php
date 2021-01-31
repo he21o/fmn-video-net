1a93eb2daa0f3e6e89ce34fcbd2bbc4bf8819d06
he21o <75876178+he21o@users.noreply.github.com>
2021-01-31 15:03:48 +0800

    Delete blob.php

--- video/watch/fmn/id/1/blob.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 返回二进制流数据
-$file_path = __DIR__ . 'http://fmn.video.sb.net.fmnworld.top/video/watch/fmn/id/1/bW7ni5flpKrlvLHniIbkuoY.mp4';
-$file_size = filesize($file_path);
-$oct_data = fread(fopen($file_path, "r"), $filesize);
- 
-header("Content-type: video/mpeg4;charset=UTF-8");
-header("Content-Length: " . $filesize);
-echo $oct_data;
